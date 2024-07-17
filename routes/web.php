fea2e3b93176acbf0541469b02b7f9e14d160e80
febriatok800 <febriatok800@gmail.com>
2024-07-17 14:31:47 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/users', function () {
+    foreach(user::all() as $user)
+    dump($user->name);
+});
